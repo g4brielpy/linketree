312f34039a4e6862d2e86df4e7095f4a4d473c15
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-03 07:22:44 -0300

    Estilização do Title1

--- src/components/Title1/index.tsx
+++ src/components/Title1/index.tsx
@@ -5,5 +5,12 @@ interface Title1Props extends React.HTMLAttributes<HTMLHeadingElement> {
 }
 
 export function Title1({ children, ...rest }: Title1Props) {
-  return <h1 {...rest}>{children}</h1>;
+  return (
+    <h1
+      {...rest}
+      className="text-3xl text-white font-extrabold md:text-4xl lg:text-5xl"
+    >
+      {children}
+    </h1>
+  );
 }
