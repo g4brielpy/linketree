99a549eca99d8801ae0605ab3568b98bf248915b
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-06 18:55:59 -0300

    Criando seção de links

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,17 @@
+import { Title1 } from "../../components/Title1";
+import { CustomLink } from "../../components/CustomLink";
+
 export function Home() {
   return (
-    <div className="bg">
-      <h1>LinkeTree Gabriel</h1>
-    </div>
+    <main className="container mx-auto p-4 mt-20 text-center">
+      <Title1>LinkeTree Gabriel</Title1>
+      <section className="max-w-2xl mx-auto">
+        <p className="text-white my-6">Veja meus links abaixo 👇🏽</p>
+        <nav className="flex flex-col gap-4">
+          <CustomLink>Meu canal do YouTube</CustomLink>
+          <CustomLink>Meu canal do WhatsApp</CustomLink>
+        </nav>
+      </section>
+    </main>
   );
 }
