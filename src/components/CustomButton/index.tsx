77d1c5d0e4ef77cd43714bcfbfa515113e53153e
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-07 22:02:09 -0300

    Estilo padrão

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -11,7 +11,19 @@ export function CustomButton({
   ...rest
 }: CustomButtonProps) {
   return (
-    <button {...rest} className={`bg-blue-600 ${className}`}>
+    // Terminar de estilizar - Deixar fonte responsiva
+    <button
+      {...rest}
+      className={`
+        bg-blue-600 text-white 
+        w-full h-10 px-5 rounded-md
+        font-semibold border-2 border-transparent
+        transition-all duration-300 ease-in-out
+        hover:border-blue-300 focus:border-blue-400
+        focus:outline-none
+        ${className}
+      `}
+    >
       {children}
     </button>
   );
