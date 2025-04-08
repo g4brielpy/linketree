549c4bcf250741960107c23903e38490025aa747
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-07 21:29:34 -0300

    Estilização completa

---
+++ src/components/CustomInput/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface CustomInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export function CustomInput({ className = "", ...props }: CustomInputProps) {
+  return (
+    <input
+      type="email"
+      className={`
+        w-full h-10 px-5 rounded-md
+        bg-gray-800 text-white 
+        font-semibold border-2 border-amber-500/40
+        transition-all duration-300 ease-in-out
+        focus:border-amber-500 focus:outline-none
+        ${className}
+      `}
+      placeholder="use@email.com"
+      {...props}
+    />
+  );
+}
