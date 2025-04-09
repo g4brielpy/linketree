d3848d8592af12fb8e3e940aaa3d986c8105a303
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-09 05:46:46 -0300

    Criando página de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,42 @@
+import LogoDevLink from "../../assets/Logo.svg";
+
+import { CustomInput } from "../../components/CustomInput";
+import { CustomButton } from "../../components/CustomButton";
+
 export function Login() {
   return (
-    <>
-      <h1>Página login</h1>
-    </>
+    <main className="container h-dvh mx-auto p-4">
+      <div className="h-full max-w-2xl mx-auto flex flex-col justify-center items-center">
+        <img
+          src={LogoDevLink}
+          alt="Dev Link"
+          className="w-[280px] md:w-[400px] h-auto"
+        />
+
+        <form action="#" className="mt-10 flex flex-col gap-4 w-full">
+          <label htmlFor="email-user" className="hidden">
+            Email
+          </label>
+          <CustomInput
+            id="email-user"
+            type="email"
+            placeholder="use@email.com"
+            required
+          />
+
+          <label htmlFor="password-user" className="hidden">
+            Senha
+          </label>
+          <CustomInput
+            id="password-user"
+            type="password"
+            placeholder="********"
+            required
+          />
+
+          <CustomButton>Acessar</CustomButton>
+        </form>
+      </div>
+    </main>
   );
 }
