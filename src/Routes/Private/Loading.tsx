60df66b0594c3432314bf1e9c3cc71c42be426f0
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-11 17:27:35 -0300

    Correção do nome e estilização

---
+++ src/Routes/Private/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <main className="h-dvh flex justify-center items-center">
+      <h1 className="text-white text-4xl font-bold">Carregando...</h1>
+    </main>
+  );
+}
