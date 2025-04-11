8957e3be0bf22e7d0cd35c8fcc592d3b84918304
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-11 11:08:23 -0300

    Fazendo a requisição para logar na apricação

--- src/utils/signIn.ts
+++ src/utils/signIn.ts
@@ -1,7 +1,18 @@
-export function login(email: string, password: string) {
-  if (email.trim() == "" || password.trim() == "") return false;
-  console.info({
-    email: email,
-    password: password,
-  });
+import { auth } from "../services/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export function login(email: string, password: string): Promise<boolean> {
+  if (email.trim() === "" || password.trim() === "") {
+    return Promise.resolve(false);
+  }
+
+  return signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log(userCredential);
+      return true;
+    })
+    .catch((e) => {
+      console.error("Erro ao fazer Login: " + e.code);
+      return false;
+    });
 }
