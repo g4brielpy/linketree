a4d1d12d55631e1423c47c4c63d1d7012f7a00cd
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-11 17:20:40 -0300

    Envolvendo rotas privadas

--- src/Routes/Private.tsx
+++ src/Routes/Private.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { auth } from "../services/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 interface PrivateProps {
   children: React.ReactNode;
 }
 
 export function Private({ children }: PrivateProps) {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      }
+
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) return <p>Carregando...</p>;
+  if (!isAuthenticated) return <p>Erro ao carregar página</p>;
+
   return <>{children}</>;
 }
