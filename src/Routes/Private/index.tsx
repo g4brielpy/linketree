20a2208fb8de17941be39e02069b06ba8a5f03be
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-11 17:23:05 -0300

    Pasta para arquivos privados

--- src/Routes/Private/index.tsx
+++ src/Routes/Private/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { auth } from "../services/firebase";
+import { auth } from "../../services/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
 interface PrivateProps {
