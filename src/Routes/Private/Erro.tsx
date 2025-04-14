393658026643487cb93d54ab85d86622e78b625e
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-14 20:20:33 -0300

    Correção nos texto

--- src/Routes/Private/Erro.tsx
+++ src/Routes/Private/Erro.tsx
@@ -4,13 +4,10 @@ export function Erro() {
   return (
     <main className="h-dvh flex justify-center items-center flex-col gap-8">
       <h1 className="text-white text-4xl font-bold">
-        Erro ao carregar a página!
+        Usuário não está logado!
       </h1>
-      <Link
-        to={"/login"}
-        className="text-xl text-blue-600 font-semibold underline"
-      >
-        Voltar para a página de Login
+      <Link to={"/login"} className="text-xl text-blue-600 underline">
+        Ir para a página de Login
       </Link>
     </main>
   );
