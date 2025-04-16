1029082cf58b8ee202ade1db025b9e5ffd101fba
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-16 14:06:07 -0300

    Corrigindo tamanho padrão do input

--- src/components/CustomInput/index.tsx
+++ src/components/CustomInput/index.tsx
@@ -8,12 +8,12 @@ export function CustomInput({ className = "", ...props }: CustomInputProps) {
     <input
       type="text"
       className={`
-        w-full h-10 px-5 rounded-md
+        ${className}
+        h-10 px-5 rounded-md
         bg-gray-800 text-white 
         font-semibold border-2 border-amber-500/40
         transition-all duration-300 ease-in-out
         focus:border-amber-500 focus:outline-none
-        ${className}
       `}
       {...props}
     />
