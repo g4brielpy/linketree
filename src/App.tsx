456ddf0df9e15ebd968417ac6b530bd693955cd6
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-23 20:36:14 -0300

    Adicionando Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes";
 
+import { Toaster } from "react-hot-toast";
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
