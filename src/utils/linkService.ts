f5a5e7a08f8f79446988a7b00dfa8f813eaca1b6
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-23 18:51:23 -0300

    Função para cadastrar links do db

--- src/utils/linkService.ts
+++ src/utils/linkService.ts
@@ -1,8 +1,22 @@
+import { db } from "../services/firebase";
+import { collection, addDoc } from "firebase/firestore";
+
 interface linkProps {
   name: string;
   url: string;
   background: string;
   color: string;
+  time?: Date;
 }
 
-export function createLink(newLink: linkProps) {}
+const path: string = "links";
+
+export async function createLink(data: linkProps): Promise<boolean> {
+  try {
+    await addDoc(collection(db, path), data);
+    return true;
+  } catch (e) {
+    console.log(`Erro ao cadastrar link: ${e}`);
+    return false;
+  }
+}
