675d4d5cc490c743d994ec6be41cf4f426fcdd2d
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-04-28 20:44:00 -0300

    Finalizando useEffect para buscar dados

--- src/hooks/useFetchLinks.ts
+++ src/hooks/useFetchLinks.ts
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
-import { getLinks } from "../utils/linkService";
+import { getLinks, linkProps } from "../utils/linkService";
 
 export function useFetchLinks() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [links, setLinks] = useState<linkProps[]>([]);
 
   useEffect(() => {
     (async () => {
       try {
-        const linksResult = await getLinks();
-        console.log(linksResult);
+        const linksResult: linkProps[] = await getLinks();
+        setLinks(linksResult);
       } catch (e) {
         throw new Error("Erro ao buscar links! Tente novamente.");
       } finally {
@@ -16,4 +17,6 @@ export function useFetchLinks() {
       }
     })();
   }, []);
+
+  return { links, isLoading };
 }
