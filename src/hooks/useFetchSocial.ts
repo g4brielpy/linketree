19a3d7c48c1e2bc98d9e7a6abea47f59a1d631f5
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-05-11 13:44:23 -0300

    UseEffect para buscar links de redes sociais

---
+++ src/hooks/useFetchSocial.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { getLinksSociais, linkSocialProps } from "../utils/linkService";
+
+export function useFetchSocial(): linkSocialProps {
+  const [socialMedia, setSocialMedia] = useState<linkSocialProps>({
+    facebook: "",
+    instagram: "",
+    youtube: "",
+  });
+  useEffect(() => {
+    const fetchSocialMedia = async () => {
+      const linksSocials: linkSocialProps = await getLinksSociais();
+      setSocialMedia(linksSocials);
+    };
+
+    fetchSocialMedia();
+  }, []);
+
+  console.log("socialMedia", socialMedia);
+
+  return socialMedia;
+}
